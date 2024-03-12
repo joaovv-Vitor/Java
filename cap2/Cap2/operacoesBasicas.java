effd0ad3411db4d6bed08487f9f238a81ce3505a
João Vitor <163120711+joaovv-Vitor@users.noreply.github.com>
2024-03-12 13:08:46 -0300

    Comparações de inteiros em Java
    Testando os métodos de Comparação de inteiros em Java.

--- cap2/Cap2/operacoesBasicas.java
+++ cap2/Cap2/operacoesBasicas.java
@@ -22,26 +22,26 @@ public class operacoesBasicas {
 
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            int numb1;
+            int numb2;
+   
+            System.out.println("Primeiro numero: ");
+            numb1 = input.nextInt();
 
-        int numb1;
-        int numb2;
-    
-        System.out.println("Primeiro numero: ");
-        numb1 = input.nextInt();
+            System.out.println("Segundo nuemro: ");
+            numb2 = input.nextInt();
 
-        System.out.println("Segundo nuemro: ");
-        numb2 = input.nextInt();
-
-        System.out.printf("%s%d%n","A soma é: ", numb1+numb2);
-        System.out.printf("%s%d%n","A subs é: ", numb1-numb2);
-        System.out.printf("%s%d%n", "A multi é: ", numb1*numb2);
-        if (numb2 != 0) {
-            System.out.printf("%s%d%n", "A div é: ", numb1/numb2);
-            System.out.printf("%s%d%n", "o resto da div é: ", numb1%numb2);    
-        }
-        else{
-            System.out.println("erro, Segundo numero é 0\n");
+            System.out.printf("%s%d%n","A soma é: ", numb1+numb2);
+            System.out.printf("%s%d%n","A subs é: ", numb1-numb2);
+            System.out.printf("%s%d%n", "A multi é: ", numb1*numb2);
+            if (numb2 != 0) {
+                System.out.printf("%s%d%n", "A div é: ", numb1/numb2);
+                System.out.printf("%s%d%n", "o resto da div é: ", numb1%numb2);    
+            }
+            else{
+                System.out.println("erro, Segundo numero é 0\n");
+            }
         }
         
 
---
+++ cap2/Comparison.java
@@ -0,0 +1,41 @@
+package cap2;
+
+import java.util.Scanner;//o import precisa ser feito antes da public class
+
+public class Comparison {
+    
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numb1;
+        int numb2;
+
+        System.out.printf("%s%n$s", "Digite dois numero", "Digite o Primeiro Número: ");
+        numb1 = input.nextInt();
+
+        System.out.println("Digite o Segundo Número: ");
+        numb2 = input.nextInt();
+
+        if (numb1>numb2) {
+            System.out.printf("%d%s%d%n", numb1, ">", numb2);
+        }
+        if (numb1<numb2) {
+            System.out.printf("%d%s%d%n", numb1, "<", numb2);
+        }
+        if (numb1==numb2) {
+            System.out.printf("%d%s%d%n", numb1, "=", numb2);
+        }
+        if (numb1>=numb2) {
+            System.out.printf("%d%s%d%n", numb1, ">=", numb2);
+        }
+        if (numb1<=numb2) {
+            System.out.printf("%d%s%d%n", numb1, "<=", numb2);
+        }
+
+        
+
+    }
+
+
+}
