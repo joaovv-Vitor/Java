b684be30bda681355d6255e552805aeec89f09d9
João Vitor <163120711+joaovv-Vitor@users.noreply.github.com>
2024-03-12 12:45:28 -0300

    Operações Básicas em java
    Testando as Operações Básicas, a ideia é familiarizar com os operadores e com a sintaxe do Java

---
+++ cap2/Cap2/operacoesBasicas.java
@@ -0,0 +1,59 @@
+package cap2.Cap2;
+
+//import java.util.*;
+import java.util.Scanner;
+
+public class operacoesBasicas {
+    /**
+     * soma +
+     * sub -
+     * div /
+     * mult *
+     * resto %
+     * 
+     * nextline != nextint
+     * 
+     * nextline pega pega a prox string
+     * nextint pega o prox int
+     * 
+     */
+
+    
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int numb1;
+        int numb2;
+    
+        System.out.println("Primeiro numero: ");
+        numb1 = input.nextInt();
+
+        System.out.println("Segundo nuemro: ");
+        numb2 = input.nextInt();
+
+        System.out.printf("%s%d%n","A soma é: ", numb1+numb2);
+        System.out.printf("%s%d%n","A subs é: ", numb1-numb2);
+        System.out.printf("%s%d%n", "A multi é: ", numb1*numb2);
+        if (numb2 != 0) {
+            System.out.printf("%s%d%n", "A div é: ", numb1/numb2);
+            System.out.printf("%s%d%n", "o resto da div é: ", numb1%numb2);    
+        }
+        else{
+            System.out.println("erro, Segundo numero é 0\n");
+        }
+        
+
+        
+
+
+
+
+
+
+    }
+
+
+}
+
---
+++ cap2/Cap2/print.java
@@ -0,0 +1,7 @@
+package cap2.Cap2;
+
+public class print {
+    public static void main(String[] args) {
+        System.out.println("null");
+    }
+}
